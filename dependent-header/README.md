e75f6a8bb3ccf71b1eac8bfc29213778c879d388
Anton Engelhardt <antoncengelhardt@icloud.com>
2024-09-04 20:25:18 +0200

    docs(dependent-header): add example
    Signed-off-by: Anton Engelhardt <antoncengelhardt@icloud.com>

---
+++ dependent-header/README.md
@@ -0,0 +1,10 @@
+# dependent-header
+
+Run `make simulate` and then visit the [header inspector](http://localhost:10000/#/Request_inspection/get_headers) in your browser
+
+You should see two headers (next to other headers) that look like this:
+
+```
+"X-Contains-Google-Apis": "true",
+"X-Contains-Some-Other-Weird-Stuff": "false",
+```
---
+++ dependent-header/src/lib.rs
@@ -0,0 +1,76 @@
+// proxy-wasm
+use log::warn;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+use std::time::Duration;
+
+// This is the initial entry point of the plugin.
+proxy_wasm::main! {{
+
+    proxy_wasm::set_log_level(LogLevel::Debug);
+
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(MyRootContext{}) });
+}}
+
+struct MyPlugin;
+
+impl HttpContext for MyPlugin {
+    fn on_http_request_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
+        // Dispatch an HTTP call to www.google.com
+        match self.dispatch_http_call(
+            "google",
+            vec![
+                (":method", "GET"),
+                (":path", "/"),
+                (":authority", "www.google.com"),
+                (":scheme", "https"),
+            ],
+            None,
+            vec![],
+            Duration::from_secs(5),
+        ) {
+            Ok(_) => Action::Pause,
+            Err(e) => {
+                warn!("Failed to dispatch_http_call: {:?}", e);
+                Action::Pause
+            }
+        }
+    }
+}
+
+impl Context for MyPlugin {
+    fn on_http_call_response(&mut self, _token_id: u32, _: usize, body_size: usize, _: usize) {
+        let _body = self
+            .get_http_call_response_body(0, body_size)
+            .unwrap_or_default();
+
+        // Convert the body to a string and log it.
+        let parsed_body = String::from_utf8_lossy(&_body);
+
+        let contains_google_apis = parsed_body.contains("googleapis");
+
+        let contains_some_other_weird_stuff = parsed_body.contains("some_other_weird_stuff");
+
+        self.add_http_request_header("x-contains-google-apis", &contains_google_apis.to_string());
+        self.add_http_request_header(
+            "x-contains-some-other-weird-stuff",
+            &contains_some_other_weird_stuff.to_string(),
+        );
+
+        self.resume_http_request();
+    }
+}
+
+pub struct MyRootContext;
+
+impl RootContext for MyRootContext {
+    fn create_http_context(&self, _context_id: u32) -> Option<Box<dyn HttpContext>> {
+        Some(Box::new(MyPlugin {}))
+    }
+
+    fn get_type(&self) -> Option<ContextType> {
+        Some(ContextType::HttpContext)
+    }
+}
+
+impl Context for MyRootContext {}
