30ea541790cb65ba00a35d437e4cbec6d20d4f8c
Anton Engelhardt <antoncengelhardt@icloud.com>
2025-06-19 10:00:17 +0200

    feat: add vs set header
    Signed-off-by: Anton Engelhardt <antoncengelhardt@icloud.com>

---
+++ duplicate-header/README.md
@@ -0,0 +1,19 @@
+# Duplicate Header
+
+Run the following command to see the result:
+
+```
+make simulate
+```
+
+```
+curl http://localhost:10000/headers -H "X-My-Header: initial value"
+```
+
+
+
+
+
+
+
+
---
+++ duplicate-header/src/lib.rs
@@ -0,0 +1,39 @@
+// proxy-wasm
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+
+// This is the initial entry point of the plugin.
+proxy_wasm::main! {{
+
+    proxy_wasm::set_log_level(LogLevel::Debug);
+
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(MyRootContext{}) });
+}}
+
+struct MyPlugin;
+
+impl HttpContext for MyPlugin {
+    fn on_http_request_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
+        // self.add_http_request_header("X-My-Header", "different-value");
+
+        self.set_http_request_header("X-My-Header", Some("different-value"));
+
+        Action::Continue
+    }
+}
+
+impl Context for MyPlugin {}
+
+pub struct MyRootContext;
+
+impl RootContext for MyRootContext {
+    fn create_http_context(&self, _context_id: u32) -> Option<Box<dyn HttpContext>> {
+        Some(Box::new(MyPlugin {}))
+    }
+
+    fn get_type(&self) -> Option<ContextType> {
+        Some(ContextType::HttpContext)
+    }
+}
+
+impl Context for MyRootContext {}
